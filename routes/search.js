6ca6448777549640d0c79f5c00e0d337d2c6c067
settleformore <ssettle93@gmail.com>
2021-06-11 10:30:40 -0500

    Added console logs

--- routes/search.js
+++ routes/search.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
     console.log(req.data, req.body)
+    console.log(req.params.track)
+    console.log(req.params.artist)
     // req.spotify.SearchTracks(track: )
     // req.spotify.SearchArtists(artist: )
     res.send("hi")
