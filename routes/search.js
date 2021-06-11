e3d3b58cf4214a75959eddf2db107c8592c78c72
settleformore <ssettle93@gmail.com>
2021-06-11 10:21:45 -0500

    naming issue on new route

--- routes/search.js
+++ routes/search.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/search', function(req, res, next) {
+router.get('/', function(req, res, next) {
     console.log(req.data, req.body)
     // req.spotify.SearchTracks(track: )
     // req.spotify.SearchArtists(artist: )
-    // res.send()
+    res.send("hi")
 })
 
 module.exports = router;
