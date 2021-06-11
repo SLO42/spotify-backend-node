3bf01ce38cb29929592db6a3cfd13c51d26c2b6b
settleformore <ssettle93@gmail.com>
2021-06-11 15:50:35 -0500

    testingg

--- routes/search.js
+++ routes/search.js
@@ -2,47 +2,48 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', async function(req, res, next) {
-    console.log(req, "req")
-    console.log(req.query, "params")
+    // console.log(req, "req")
+    // console.log(req.query, "params")
 
     let trackData = {};
     let artistData = {};
 
-    req.spotify.searchTracks(`track:${req.query.track}`)
+    trackData = await req.spotify.searchTracks(`track:${req.query.track}`)
     .then((response) => {
         console.log('track', response)
         if (response.status === 200) {
 
-            trackData = response.data
+            return response.data;
         }
         // res.status(200).send(response)
     })
     .catch(error = () => {
         console.error(error);
-        res.status(408).send(error)
+        // res.status(408).send(error)
+        return null;
     })
     
-    req.spotify.searchArtist(`artist:${req.query.artist}`)
-    .then((response) => {
-        console.log('artist', response)
-        if (response.status === 200) {
+    // req.spotify.searchArtist(`artist:${req.query.artist}`)
+    // .then((response) => {
+    //     console.log('artist', response)
+    //     if (response.status === 200) {
 
-            artistData = response.data
-        }
-        // res.status(200).send(response)
-    })
-    .catch(error = () => {
-        console.error(error);
-        res.status(408).send(error)
-    })
+    //         artistData = response.data
+    //     }
+    //     // res.status(200).send(response)
+    // })
+    // .catch(error = () => {
+    //     console.error(error);
+    //     res.status(408).send(error)
+    // })
 
 
 
     // })
     // if (trackData and artistData !== null or empty)
     console.log('trackData', trackData)
-    console.log('artistData', artistData)
-    res.send(trackData, artistData)
+    // console.log('artistData', artistData)
+    res.send(trackData)
 })
 
 module.exports = router;
