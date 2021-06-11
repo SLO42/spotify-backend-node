1482aca58a20078c66d416c8a4902af84a098525
settleformore <ssettle93@gmail.com>
2021-06-11 11:02:57 -0500

    Request built

--- routes/search.js
+++ routes/search.js
@@ -1,13 +1,47 @@
 var express = require('express');
+const { response } = require('../app');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
     console.log(req.data, req.body)
     console.log(req.params.track)
     console.log(req.params.artist)
-    // req.spotify.SearchTracks(track: )
-    // req.spotify.SearchArtists(artist: )
-    res.send("hi")
+    let trackData;
+    let artistData;
+
+    trackData = req.spotify.SearchTracks(`track:${req.params.track.name}`)
+    .then(response = () => {
+        console.log(response)
+        return response;
+    })
+    .catch(error = () => {
+        console.error(error);
+    })
+    // req.spotify.SearchTracks(`track:${req.params.track.name}`)
+    // .then(response = () => {
+    //     req.spotify.SearchArtists(`artist:${req.params.artist.name}`)
+    //     .then(response = () => {
+
+    //     })
+    // })
+    // .catch(error = () => {
+    //     console.log(error)
+    // })
+
+
+    // req.spotify.SearchTracks(`track:${req.params.track.name}`)
+    // .then((responseOne) => {
+    //     req.spotify.SearchArtists(`artist:${req.params.artist.name}`)
+    //     .then((responseTwo) => {
+
+    //     })
+    //     .catch((error))
+
+    // })
+    // .catch((error) => {
+
+    // })
+    res.send(trackData)
 })
 
 module.exports = router;
