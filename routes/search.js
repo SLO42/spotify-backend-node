d14f863e15a07fba6afb48028957628af2c05046
settleformore <ssettle93@gmail.com>
2021-06-14 15:52:40 -0500

    Tested and working search return

--- routes/search.js
+++ routes/search.js
@@ -9,49 +9,25 @@ router.get('/', async function(req, res, next) {
     let artistData = {};
 
     const tracks = await req.spotify.searchTracks(`track:${req.query.track}`)
-    .then( async (response1 ) => {
-        console.log('t - start', response1, "t - end")
+    .then((response1 ) => {
+        // console.log('t - start', response1, "t - end")
         return response1.body.tracks.items;
-        // res.status(200).send({tracks: tracks})
     })
     .catch(error = () => {
-        console.error(error);
+        // console.error(error);
         res.status(408).send(error)
         return null;
     })
     const artist = await req.spotify.searchArtists(`artist:${req.query.artist}`)
     .then((response2) => {
-        console.log("Artist - Start", response2, "Artist - End")
+        // console.log("Artist - Start", response2, "Artist - End")
         return response2.body
-        // res.status(200).send([tracks, artist])
     })
     .catch(error = () => {
-        console.error(error);
+        // console.error(error);
         res.status(408).send(error)
     })
     res.status(200).send([tracks, artist])
-    
-    // req.spotify.searchArtist(`artist:${req.query.artist}`)
-    // .then((response) => {
-    //     console.log('artist', response)
-    //     if (response.status === 200) {
-
-    //         artistData = response.data
-    //     }
-    //     // res.status(200).send(response)
-    // })
-    // .catch(error = () => {
-    //     console.error(error);
-    //     res.status(408).send(error)
-    // })
-
-
-
-    // })
-    // if (trackData and artistData !== null or empty)
-    // console.log('trackData', trackData)
-    // console.log('artistData', artistData)
-    // res.send(trackData)
 })
 
 module.exports = router;
